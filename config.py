252e4b72db4919c7d646f7f4a106dc87bf6e28aa
marcoferrari92 <74426411+marcoferrari92@users.noreply.github.com>
2026-04-08 11:57:27 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 # config.py
 
+# Parametri di default
+Giudizio_AI = 1.0
+
 # Testi di default
 TXT_DEFAULT_CAMPAIGN = """Newsletter che spiega nuovi DPI indispensabili per lo stoccaggio di materiali pericolosi o tossici, quali prodotti da reazioni chimiche, per un corso di formazione organizzato a Vicenza."""
 
